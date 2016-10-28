6ff17353d12379e616dd52033507bc8159201599
Takeru Sato <midium.size@gmail.com>
2016-10-28 20:05:30 +0900

    Modify run script

--- run.sh
+++ run.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-if [ "${CONF_FILE_URL}" != "" ]; then
-  cd /etc/google-fluentd/config.d && curl -sSO ${CONF_FILE_URL} && cd -
+if [ "${CONF_FILE_URLS}" != "" ]; then
+  mkdir -p /etc/google-fluentd/config.d
+  cd /etc/google-fluentd/config.d
+  for url in ${CONF_FILE_URLS}; do
+    curl -sSO $url -H "Metadata-Flavor: Google"
+  done
+  # for f in $(ls); do
+  #   mv ${f} ${f}.conf
+  # done
+  cd -
 fi
 
 exec google-fluentd
