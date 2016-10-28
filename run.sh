abe9faf2151ad81fd3edd836cab7627c92456f21
Takeru Sato <midium.size@gmail.com>
2016-10-28 20:20:42 +0900

    Add .conf suffix to download file

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ if [ "${CONF_FILE_URLS}" != "" ]; then
   for url in ${CONF_FILE_URLS}; do
     curl -sSO $url -H "Metadata-Flavor: Google"
   done
-  # for f in $(ls); do
-  #   mv ${f} ${f}.conf
-  # done
+  for f in $(ls); do
+    mv ${f} ${f}.conf
+  done
   cd -
 fi
 
